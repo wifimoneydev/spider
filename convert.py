4bf26af5e2c0153e15efb4247c30e2c8ca1fa430
Daniel Awofadeju <daniel@Daniels-MacBook-Air.local>
2025-05-07 08:35:40 +0100

    Updated convert.py to support Google Sheets public URL input

--- convert.py
+++ convert.py
@@ -2,16 +2,16 @@ import pandas as pd  # lib for excel
 import os  # to read file names
 
 # This function converts Excel to JSON and JSONL
-def convert_excel_to_json(file_name):
+def convert_excel_to_json(input_file, output_dir):
     # Load the Excel file into a table
-    data = pd.read_excel(file_name)
+    data = pd.read_excel(input_file)
 
     # Get just the file name (no .xlsx)
-    file_base = os.path.splitext(file_name)[0]
+    file_base = os.path.splitext(os.path.basename(input_file))[0]
 
     # Create new file names for the outputs
-    json_output = file_base + ".json"
-    jsonl_output = file_base + ".jsonl"
+    json_output = os.path.join(output_dir, file_base + ".json")
+    jsonl_output = os.path.join(output_dir, file_base + ".jsonl")
 
     # Save everything to one JSON file 
     data.to_json(json_output, orient="records", indent=4)
@@ -25,6 +25,9 @@ def convert_excel_to_json(file_name):
     print("-", json_output)
     print("-", jsonl_output)
 
-
-#  Run the function with your file name
-convert_excel_to_json("data2.xlsx")
+# Run the function with your input file path and output directory
+# replace doc id with yours 
+input_file = "https://docs.google.com/spreadsheets/d/<1Uz_XzfKg4cxFVLq_uXM1j7cQijjMqExI7pzufmEzAwU>/export?format=xlsx"
+#replace output dir with your path
+output_dir = "/Users/desktop/output"
+convert_excel_to_json(input_file, output_dir)
